3323828fa6d9d6e553c08cba952f5ff8b62815dd
Tousif Tanjim Anan <45902297+ananan37326@users.noreply.github.com>
2022-01-06 15:01:00 +0600

    Add files via upload

--- Offline2/test.cpp
+++ Offline2/test.cpp
@@ -4,7 +4,7 @@
 #include<unistd.h>
 #include"passenger.h"
 
-#define MAX_PASSENGERS 10
+#define MAX_PASSENGERS 5
 #define TOTAL_TIME 45
 #define LEFT_TO_RIGHT 1
 #define RIGHT_TO_LEFT -1
@@ -20,6 +20,7 @@ int M,N,P,w,x,y,z;
 int global_time = 0;
 int current_time = 0;
 int vip_count = 0;
+int lost_count = 0;
 
 // Passenger and thread arrays
 Passenger passengers[MAX_PASSENGERS];
@@ -37,10 +38,16 @@ sem_t* belt_sem; // for availanility in each belt
 
 void *timeCount(void *arg)
 {
+    // while (true)
+    // {
+    //     sleep(1);
+    //     global_time++;
+    // }
+    clock_t start = clock();
     while (true)
     {
-        sleep(1);
-        global_time++;
+        clock_t end = clock();
+        global_time = (round)(end - start) / CLOCKS_PER_SEC;
     }
 }
 
@@ -82,10 +89,9 @@ void initMutexSemaphores(){
 }
 
 void setPassengersInfo(){
-    srand(time(NULL));
     
     double lambda = TOTAL_TIME / (1.0 * MAX_PASSENGERS);
-    int arrival_count = 0;
+    int arrival_count = global_time;
     default_random_engine random_engine;
     poisson_distribution<int> poisson(lambda);
 
@@ -96,13 +102,25 @@ void setPassengersInfo(){
         arrival_count = arrival_count + new_arrival;
         
         Passenger new_passenger(i+1);
+        //sleep(new_arrival);
+
         new_passenger.setArrivalTime(arrival_count);
         int setVIPStatus = rand()%2;
-        new_passenger.setVIP(setVIPStatus);
+        if(setVIPStatus == 1){
+            new_passenger.setVIP(true);
+            new_passenger.addName(" (VIP)");
+            printf("%s\n",new_passenger.getName().c_str());
+        }
+        else{
+            new_passenger.setVIP(false);
+        }
+        //new_passenger.setVIP(setVIPStatus);
         new_passenger.setKioskNumber(rand()%M);
         new_passenger.setBeltNumber(rand()%N);
 
         passengers[i] = new_passenger;
+
+
     }
 }
 
@@ -112,13 +130,13 @@ void selfCheckin(Passenger* passenger){
     pthread_mutex_lock(&kiosk_mutex[kiosk_number]);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has started self-check in at kiosk %d at time %d\n",passenger->getName(),kiosk_number,global_time);
+    printf("%s has started self-check in at kiosk %d at time %d\n",passenger->getName().c_str(),kiosk_number,global_time);
     pthread_mutex_unlock(&print_mutex);
 
     sleep(w);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has finished check-in at time %d\n",passenger->getName(),global_time);
+    printf("%s has finished check-in at time %d\n",passenger->getName().c_str(),global_time);
     pthread_mutex_unlock(&print_mutex);
 
     pthread_mutex_unlock(&kiosk_mutex[kiosk_number]);
@@ -128,19 +146,19 @@ void securityCheck(Passenger* passenger){
     int belt_number = passenger->getBeltNumber();
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has started waiting for security check in in belt %d from time %d\n",passenger->getName(),belt_number, global_time);
+    printf("%s has started waiting for security check in in belt %d from time %d\n",passenger->getName().c_str(),belt_number, global_time);
     pthread_mutex_unlock(&print_mutex);
 
     sem_wait(&belt_sem[belt_number]);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has started the security check at time %d\n",passenger->getName(), global_time);
+    printf("%s has started the security check at time %d\n",passenger->getName().c_str(), global_time);
     pthread_mutex_unlock(&print_mutex);
 
     sleep(x);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has crossed the security check at time %d\n",passenger->getName(), global_time);
+    printf("%s has crossed the security check at time %d\n",passenger->getName().c_str(), global_time);
     pthread_mutex_unlock(&print_mutex);
 
     sem_post(&belt_sem[belt_number]);
@@ -150,7 +168,7 @@ void startBoarding(Passenger* passenger){
     pthread_mutex_lock(&boarding_mutex);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has started boarding the plane at time %d\n",passenger->getName(), global_time);
+    printf("%s has started boarding the plane at time %d\n",passenger->getName().c_str(), global_time);
     pthread_mutex_unlock(&print_mutex);
 
     int hasLostBoardingPass = rand()%2;
@@ -162,7 +180,7 @@ void startBoarding(Passenger* passenger){
     sleep(y);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has boarded the plane at time %d\n",passenger->getName(), global_time);
+    printf("%s has boarded the plane at time %d\n",passenger->getName().c_str(), global_time);
     pthread_mutex_unlock(&print_mutex);
 
     pthread_mutex_unlock(&boarding_mutex);
@@ -173,23 +191,56 @@ void goToSpecialKiosk(Passenger* passenger){
     pthread_mutex_lock(&special_kiosk_mutex);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has started self-check in at special kiosk at time %d\n",passenger->getName(), global_time);
+    printf("%s has started self-check in at special kiosk at time %d\n",passenger->getName().c_str(), global_time);
     pthread_mutex_unlock(&print_mutex);
 
     sleep(w);
 
     pthread_mutex_lock(&print_mutex);
-    printf("%s has finished check-in at time %d\n",passenger->getName(),global_time);
+    printf("%s has finished check-in at time %d\n",passenger->getName().c_str(),global_time);
     pthread_mutex_unlock(&print_mutex);
 
     pthread_mutex_unlock(&special_kiosk_mutex);
 }
 
-void goToVIPChannelForward(Passenger* passenger){
+
+void goToVIPChannelLefttoRight(Passenger* passenger){
+    //printf("VIP forwards\n");
+    vip_count++;
+    if(vip_count == 1){
+        pthread_mutex_lock(&vip_mutex);
+        pthread_mutex_lock(&channel_mutex);
+    }
+
+    pthread_mutex_lock(&print_mutex);
+    printf("%s has crossed the VIP channel to the boarding gate at time %d\n",passenger->getName().c_str(), global_time);
+    pthread_mutex_unlock(&print_mutex);
+
+    vip_count--;
+    if(vip_count == 0){
+        pthread_mutex_unlock(&channel_mutex);
+        pthread_mutex_unlock(&vip_mutex);
+    }
 
 }
 
-void goToVIPChannelBackward(Passenger* passenger){
+void goToVIPChannelRighttoLeft(Passenger* passenger){
+    //printf("VIP Backwards\n");
+    pthread_mutex_lock(&vip_mutex);
+    pthread_mutex_unlock(&vip_mutex);
+    lost_count++;
+    if(lost_count == 1){
+        pthread_mutex_lock(&channel_mutex);
+    }
+
+    pthread_mutex_lock(&print_mutex);
+    printf("%s has used the VIP channel to go to the special kiosk at time %d\n",passenger->getName().c_str(), global_time);
+    pthread_mutex_unlock(&print_mutex);
+
+    lost_count--;
+    if(lost_count == 0){
+        pthread_mutex_unlock(&channel_mutex);
+    }
 
 }
 
@@ -199,7 +250,7 @@ void* arrivalAtAirport(void* passenger){
     selfCheckin(curr_passenger);
 
     if(curr_passenger->isVIP()){
-        // TO-DO
+        goToVIPChannelLefttoRight(curr_passenger);
     }
     else{
         securityCheck(curr_passenger);
@@ -208,24 +259,35 @@ void* arrivalAtAirport(void* passenger){
     while(!curr_passenger->hasBoarded()){
         startBoarding(curr_passenger);
         if(!curr_passenger->hasBoardingPass()){
-            goToVIPChannelBackward(curr_passenger);
+            goToVIPChannelRighttoLeft(curr_passenger);
             goToSpecialKiosk(curr_passenger);
-            goToVIPChannelForward(curr_passenger);
+            goToVIPChannelLefttoRight(curr_passenger);
         }
     }
+
+    startBoarding(curr_passenger);
 }
 
 void startProcess(){
-    global_time = passengers[0].getArrivaltime();
     for (int i = 0; i < MAX_PASSENGERS; i++)
     {
         /* code */
         Passenger this_passenger = passengers[i];
-        pthread_create(&thread_passengers[i],NULL, arrivalAtAirport, (void*) &this_passenger);
 
         pthread_mutex_lock(&print_mutex);
-        printf("%s has arrived ar the airport at time %d \n",this_passenger.getName(),this_passenger.getArrivaltime());
+        //printf("%s has arrived ar the airport at time %d \n",this_passenger.getName().c_str(),this_passenger.getArrivaltime());
+        printf("%s has arrived ar the airport at time %d \n",this_passenger.getName().c_str(),global_time);
         pthread_mutex_unlock(&print_mutex);
+
+        pthread_create(&thread_passengers[i],NULL, arrivalAtAirport, (void*) &this_passenger);
+
+        if(i != MAX_PASSENGERS-1){
+            sleep(passengers[i+1].getArrivaltime() - passengers[i].getArrivaltime());
+        }
+
+        
+
+        
     }
 
     for (int i = 0; i < MAX_PASSENGERS; i++)
@@ -237,15 +299,69 @@ void startProcess(){
     
 }
 
+void createPassengerThreads(){
+    double lambda = TOTAL_TIME / (1.0 * MAX_PASSENGERS);
+    int arrival_time = global_time;
+    default_random_engine random_engine;
+    poisson_distribution<int> poisson(lambda);
+
+    for (int i = 0; i < MAX_PASSENGERS; i++)
+    {
+        /* code */
+        int new_arrival_time = poisson(random_engine);
+        arrival_time = arrival_time + new_arrival_time;
+        
+        Passenger new_passenger(i+1);
+        sleep(new_arrival_time);
+
+        new_passenger.setArrivalTime(arrival_time);
+        int setVIPStatus = rand()%2;
+        if(setVIPStatus == 1){
+            new_passenger.setVIP(true);
+            new_passenger.addName(" (VIP)");
+            printf("%s\n",new_passenger.getName().c_str());
+        }
+        else{
+            new_passenger.setVIP(false);
+        }
+        //new_passenger.setVIP(setVIPStatus);
+        new_passenger.setKioskNumber(rand()%M);
+        new_passenger.setBeltNumber(rand()%N);
+
+        passengers[i] = new_passenger;
+
+        pthread_mutex_lock(&print_mutex);
+        //printf("%s has arrived ar the airport at time %d \n",this_passenger.getName().c_str(),this_passenger.getArrivaltime());
+        printf("%s has arrived ar the airport at time %d \n",new_passenger.getName().c_str(),global_time);
+        pthread_mutex_unlock(&print_mutex);
+
+        pthread_create(&thread_passengers[i],NULL, arrivalAtAirport, (void*) new_passenger);
+
+    }
+
+    for (int i = 0; i < MAX_PASSENGERS; i++)
+    {
+        /* code */
+        pthread_join(thread_passengers[i],NULL);
+    }
+}
+
 int main(){
+    srand(time(NULL));
+
     pthread_t timer_thread;
 
     readFile();
-    //printf("%d %d %d\n",M,N,P);
-    //printf("%d %d %d %d\n",w,x,y,z);
+    printf("%d %d %d\n",M,N,P);
+    printf("%d %d %d %d\n",w,x,y,z);
+    pthread_create(&timer_thread,NULL, timeCount, NULL);
+
     initMutexSemaphores();
-    setPassengersInfo();
-    startProcess();
+    //setPassengersInfo();
+    //global_time = passengers[0].getArrivaltime();
+    //startProcess();
+
+    createPassengerThreads();
 
 
     return 0;
